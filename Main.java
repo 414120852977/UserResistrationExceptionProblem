7ddc0f1a6c87d18096dd72ad5e1b301b2c2022bc
Ashok Mane <ashokmane81800@gmail.com>
2021-04-19 13:40:48 +0530

    [ashok] created a main file for call objects

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class Main {
+    public static void main(String[] args) {
+        UserResistration userResistration = new UserResistration();
+        userResistration.checkFirstName();
+        userResistration.checkLastName();
+        userResistration.checkEmailId();
+        userResistration.checkPhoneNo();
+        userResistration.checkPassword();
+    }
+}
